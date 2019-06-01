354c3171613f1d1c89f5d371d9497ea4ceef104d
Guido Balsells <guidobalsells@gmail.com>
2019-06-01 13:37:44 -0300

    Se obtienen y se pasan recetas por props

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.scss';
 
-const RecipeCard = () => {
+const RecipeCard = (recipe) => {
+  console.log('Receta: ', recipe.title, recipe.main_image.url)
   return (
     <React.Fragment>
-    pijazo
+    <div className="card">
+    <img src={recipe.main_image.url} alt="Avatar" />
+      <div className="container">
+        <h4><b>{recipe.title}</b></h4> 
+        <p>Architect & Engineer</p> 
+      </div>
+    </div>
     </React.Fragment>
   )
 }
