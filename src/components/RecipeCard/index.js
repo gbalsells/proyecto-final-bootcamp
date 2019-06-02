f863780dbe45f5b12c5277cf964602fa554a98cb
Guido Balsells <guidobalsells@gmail.com>
2019-06-02 12:31:45 -0300

    rating & prep_time recipeCard

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -10,7 +10,35 @@ const RecipeCard = (recipe) => {
         <img src={recipe.main_image.url} alt="recipe"></img>
       </Link>
       <div className="recipe-card__container">
-        <h1>{recipe.title}</h1> 
+        <h1>{recipe.title}</h1>
+        <div className="recipe-card__container__info">
+          {
+            recipe.rating === 1 ? 
+              <div>
+                <a href="star" className="recipe-card__container__info__fullStar">⋆</a>
+                <a href="star" className="recipe-card__container__info__emptyStar">⋆⋆⋆⋆</a>
+              </div> :
+            recipe.rating === 2 ? 
+              <div>
+                <a href="star" className="recipe-card__container__info__fullStar">⋆⋆</a>
+                <a href="star" className="recipe-card__container__info__emptyStar">⋆⋆⋆</a>
+              </div> :
+            recipe.rating === 3 ? 
+            <div>
+              <a href="star" className="recipe-card__container__info__fullStar">⋆⋆⋆</a>
+              <a href="star" className="recipe-card__container__info__emptyStar">⋆⋆</a>
+              </div> :
+            recipe.rating === 4 ? 
+            <div>
+              <a href="star" className="recipe-card__container__info__fullStar">⋆⋆⋆⋆</a>
+              <a href="star" className="recipe-card__container__info__emptyStar">⋆</a>
+            </div> : 
+            <div>
+              <a href="star" className="recipe-card__container__info__fullStar">⋆⋆⋆⋆⋆</a>
+            </div>
+          }
+          <i className="fa fa-clock-o" > {recipe.prep_time} </i>
+        </div>
       </div>
     </div>
     </React.Fragment>
